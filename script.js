0b1cd33ac8bdfd63b2fadb7ac22ea151208059ff
Nwabueze Okemini <122814541+NwabuezeM@users.noreply.github.com>
2023-07-15 16:42:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
+// JavaScript code
 const animeBtn = document.getElementById("anime-btn");
 const animeContainer = document.getElementById("anime-container");
 const animeImage = document.getElementById("anime-image");
 const animeName = document.getElementById("anime-name");
 const errorMessage = document.getElementById("error-message");
+const downloadLink = document.getElementById("download-link");
 
-const getImage = 
 animeBtn.addEventListener("click", async function() {
-    try {
-        errorMessage.style.display = "none";
-        animeContainer.style.display = "block";
-        animeImage.src = "spinner.svg";
-        animeName.innerText = "Anime Name";
-        animeBtn.innerText = "Loading...";
-        animeBtn.classList.replace("anime-btn", "active-btn");
-        animeBtn.disabled = true;
-        const response = await fetch("https://api.catboys.com/img")
-        const data = await response.json();
-        console.log(data);
-        animeContainer.style.display = "block";
-        animeImage.src = data.url;
-        animeName.innerText = data.artist;
-        animeBtn.innerText = "Get Anime";
-        animeBtn.disabled = false;
-        animeBtn.classList.replace("active-btn", "anime-btn");
-    } 
-    catch (error) {
-        animeContainer.style.display = "none";
-        animeBtn.disabled = false;
-        errorMessage.style.display = "block";
-        errorMessage.innerText = "An error occurred, please try again"
-        animeBtn.classList.replace("active-btn", "anime-btn");
-        animeBtn.innerText = "Get Anime";
-    }
-})
+  try {
+    errorMessage.style.display = "none";
+    animeContainer.style.display = "block";
+    animeImage.src = "spinner.svg";
+    animeName.innerText = "Anime Name";
+    animeBtn.innerText = "Loading...";
+    animeBtn.classList.replace("anime-btn", "active-btn");
+    animeBtn.disabled = true;
+    const response = await fetch("https://api.catboys.com/img")
+    const data = await response.json();
+    console.log(data);
+    animeContainer.style.display = "block";
+    animeImage.src = data.url;
+    animeName.innerText = data.artist;
+    animeBtn.innerText = "Get Anime";
+    animeBtn.disabled = false;
+    animeBtn.classList.replace("active-btn", "anime-btn");
+  } 
+  catch (error) {
+    animeContainer.style.display = "none";
+    animeBtn.disabled = false;
+    errorMessage.style.display = "block";
+    errorMessage.innerText = "An error occurred, please try again"
+    animeBtn.classList.replace("active-btn", "anime-btn");
+    animeBtn.innerText = "Get Anime";
+  }
+});
